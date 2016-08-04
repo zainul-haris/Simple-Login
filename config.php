fe22ce32e30c12f04b9a96dea4d95c2bf5e9953d
zainul-haris <zainulharissaintek@gmail.com>
2016-08-04 14:55:44 +0700

    source code

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+	// 
+	$servername = "localhost";
+	$username 	= "root";
+	$password 	= "";
+	$nama_db 	= "test_login_db";
+
+	$koneksi = mysqli_connect($servername, $username, $password, $nama_db);
+?>
