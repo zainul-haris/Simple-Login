9f5d396682aaf4c02099a019fecd35abd1092abb
zainul-haris <zainulharissaintek@gmail.com>
2016-08-04 14:57:04 +0700

    source code

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title></title>
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-default fixed-top">
+			  <div class="container-fluid">
+				    <div class="navbar-header">
+				      	<a class="navbar-brand" href="#">Sample Login</a>
+				    </div>
+				
+				    <ul class="nav navbar-nav navbar-right">
+					      <li><a href="logout.php">Log out</a></li>
+				    </ul>
+			  </div>
+	    </nav>
+
+	    <nav class="navbar navbar-default navbar-fixed-bottom">
+	    	<p class='text-center'><em>Powered By <strong> Zainul Haris</strong></em>			 
+			<br></p><h6 class="text-center">Copyright &copy; 2016</h6>
+	    </nav>
+
+	</body>
+</html>
+
+
